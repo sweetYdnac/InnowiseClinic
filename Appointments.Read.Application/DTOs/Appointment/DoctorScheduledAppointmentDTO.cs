b8eb56c7092ec7c3a125515aaf199d023f8c06f9
Evgeny Koreba <96253105+sweetYdnac@users.noreply.github.com>
2023-03-08 14:18:42 +0300

    Add additional properties to doctorScheduleResponse

--- Appointments.Read.Application/DTOs/Appointment/DoctorScheduledAppointmentDTO.cs
+++ Appointments.Read.Application/DTOs/Appointment/DoctorScheduledAppointmentDTO.cs
@@ -6,6 +6,9 @@
         public TimeOnly EndTime { get; set; }
         public Guid PatientId { get; set; }
         public string PatientFullName { get; set; }
+        public DateOnly PatientDateOfBirth { get; set; }
+        public string DoctorFullName { get; set; }
+        public string DoctorSpecializationName { get; set; }
         public string ServiceName { get; set; }
         public bool IsApproved { get; set; }
         public Guid? ResultId { get; set; }
