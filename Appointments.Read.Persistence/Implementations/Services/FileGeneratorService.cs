0a348f2c9e6dda1def8a5ea147c0029b024b226b
Evgeny Koreba <96253105+sweetYdnac@users.noreply.github.com>
2023-03-10 11:24:43 +0300

    Add feature implementation

--- Appointments.Read.Persistence/Implementations/Services/FileGeneratorService.cs
+++ Appointments.Read.Persistence/Implementations/Services/FileGeneratorService.cs
@@ -2,58 +2,26 @@
 using Appointments.Read.Application.Interfaces.Services;
 using DinkToPdf;
 using HandlebarsDotNet;
+using Microsoft.Extensions.Configuration;
 using Shared.Models.Response.Appointments.AppointmentResult;
 
 namespace Appointments.Read.Persistence.Implementations.Services
 {
     public class FileGeneratorService : IFileGeneratorService
     {
-        //public PdfResultResponse GetPdfAppointmentResult(PdfResultDTO dto)
-        //{
-        //    var doc = new PdfDocument();
-        //    var page = doc.Pages.Add();
-        //    var pdfGrid = new PdfGrid();
-        //    var data = new List<object>()
-        //    {
-        //        new { Id = "Full Name", Name = dto.PatientFullName },
-        //        new { Id = "Patient date of birth", Name = dto.PatientDateOfBirth },
-        //        new { Id = "Doctor full name", Name = dto.DoctorFullName },
-        //        new { Id = "Doctor specialization", Name = dto.DoctorSpecializationName },
-        //        new { Id = "Service", Name = dto.ServiceName },
-        //        new { Id = "Complaints", Name = dto.Complaints },
-        //        new { Id = "Conclusion", Name = dto.Conclusion },
-        //        new { Id = "Recommendations", Name = dto.Recommendations },
-        //    };
+        private readonly IConfiguration _configuration;
 
-        //    pdfGrid.DataSource = data;
-        //    pdfGrid.Draw(page, new Syncfusion.Drawing.PointF(10, 10));
-
-        //    using (var stream = new MemoryStream())
-        //    {
-        //        doc.Save(stream);
-        //        stream.Position = 0;
-        //        doc.Close();
-
-        //        return new PdfResultResponse
-        //        {
-        //            Content = stream.ToArray(),
-        //            ContentType = "application/pdf",
-        //            FileName = $"{dto.Date:hh:mm - yyyy-MM-dd} - {dto.PatientFullName}.pdf"
-        //        };
-        //    }
-        //}
+        public FileGeneratorService(IConfiguration configuration) => _configuration = configuration;
 
         public async Task<PdfResultResponse> GetPdfAppointmentResult(PdfResultDTO dto)
         {
-            //var path = "/app/Appointments.Read.Application/FileTemplates/PdfTemplate.html";
-            var path = "src/PdfTemplate.html";
+            var path = _configuration.GetValue<string>("PdfTemplate:HtmlPath");
 
             var html = await File.ReadAllTextAsync(path);
             var handleBars = Handlebars.Compile(html);
             var parsed = handleBars(dto);
 
             var converter = new SynchronizedConverter(new PdfTools());
-
             var document = new HtmlToPdfDocument()
             {
                 GlobalSettings =
@@ -66,7 +34,11 @@ namespace Appointments.Read.Persistence.Implementations.Services
                     new ObjectSettings()
                     {
                         HtmlContent = parsed,
-                        WebSettings = { DefaultEncoding = "utf-8" },
+                        WebSettings =
+                        {
+                            DefaultEncoding = "utf-8",
+                            UserStyleSheet = _configuration.GetValue<string>("PdfTemplate:CssPath")
+                        },
                     }
                 }
             };
